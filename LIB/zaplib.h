6fdd53b9ec0a583590fa8116d7b062eb3d8c6fed
nesdoug <dougfraker@gmail.com>
2020-07-13 23:34:22 -0400

    update
    added a 2nd zapper read function.

--- LIB/zaplib.h
+++ LIB/zaplib.h
@@ -1,13 +1,28 @@
-// written by Doug Fraker 2019
+// written by Doug Fraker 2019-2020
 // NES zapper gun code
 
 // pass it 0 or 1, which port
 // returns 0 or 1 if trigger pulled
 unsigned char __fastcall__ zap_shoot(unsigned char pad);
 
+
 // pass it 0 or 1, which port
 // returns 0 or 1 if detects light this frame
 unsigned char __fastcall__ zap_read(unsigned char pad);
+
+
+// alt. version from "Falling Tiles" game.
+// always reads from port 2
+// pass it 1-224, NOT zero
+// = approximately how many scanlines to keep reading
+// note, PAL will need a little more than NTSC
+// allows you to avoid lag frames on misses.
+// "Falling Tiles" used 196 for shooting low objects
+// and 176 for shooting objects that are higher
+// and should exit this loop before the end of the frame.
+
+// returns 0 or 1 if detects light this frame
+unsigned char __fastcall__ zap_read2(unsigned char pad);
 	
 	
 	
--- LIB/zaplib.s
+++ LIB/zaplib.s
@@ -1,8 +1,8 @@
-;written by Doug Fraker 2019
+;written by Doug Fraker 2019-2020
 ;NES zapper gun code
 
 
-.export _zap_shoot, _zap_read
+.export _zap_shoot, _zap_read, _zap_read2
 
 
 
@@ -73,6 +73,59 @@ _zap_read:
 	
 
 	
+; changed version, now the a = scanlines to check
+; a should be 1-224, NOT ZERO
 	
+;value vs real (scanlines)
+;16 = 17
+;32 = 32
+;64 = 63
+;100 = 99
+;128 = 127
+;200 = 198
+;224 = 221
+	
+	
+_zap_read2:
+	ldx #0
+	stx <TEMP
+	asl a
+	rol <TEMP
+	asl a
+	rol <TEMP
+
+	tax
+	beq @zero
+;x is not zero, will exit early, add 1
+	inc <TEMP
+@zero:	
+	ldy #1
+@loop:
+	lda #1
+	sta CTRL_PORT1
+	lda #0
+	sta CTRL_PORT1
+	lda CTRL_PORT1,y
+	and #$08
+	beq @hit_yes ;0 = light, 8 = no light
+
+	ora $00 ; waste 3 cycles
+
+	dex
+	bne @loop
+
+	dec <TEMP
+	bmi @hit_no ;if started zero, rolled to ff, exit
+	bne @loop ;positive, but above zero
+	
+@hit_no:
+	lda #0
+	tax
+	rts
+	
+@hit_yes:
+	lda #1
+	ldx #0
+	rts	
 	
 	
